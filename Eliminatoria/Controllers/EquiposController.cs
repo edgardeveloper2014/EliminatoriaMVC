829a16df893bd813ab0645015471785985e9516f
edgardeveloper <edgar.developer@hotmail.com>
2017-11-03 18:59:59 -0500

    noviembre 3 del 2017 656pm Equipo
    realize el codigo de registro y listado de equipos  con entity framework 5.0 y MVC

---
+++ Eliminatoria/Controllers/EquiposController.cs
@@ -0,0 +1,31 @@
+﻿using Eliminatoria.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eliminatoria.Controllers
+{
+    public class EquiposController : Controller
+    {
+        private partidoDBEntities1 db = new Models.partidoDBEntities1();
+        // GET: Equipos
+        public ActionResult registro()
+        {          
+            return View(db.equipo.ToList());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult registro(equipo objequipo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.equipo.Add(objequipo);
+                db.SaveChanges();
+                return RedirectToAction("registro");
+            }
+            return View(objequipo);
+        }
+    }
+}
